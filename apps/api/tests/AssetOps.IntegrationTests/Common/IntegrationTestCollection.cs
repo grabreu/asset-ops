ec31fcca36ca27baa0df03911b6df05c89531978
grabreu <gabriel.roberto.abreu@outlook.com.br>
2026-08-22 10:48:41 -0300

    chore(api): scaffold minimal API project (#3)

---
+++ apps/api/tests/AssetOps.IntegrationTests/Common/IntegrationTestCollection.cs
@@ -0,0 +1,7 @@
+namespace AssetOps.IntegrationTests.Common;
+
+[CollectionDefinition(Name)]
+public sealed class IntegrationTestCollection : ICollectionFixture<ApiFactory>
+{
+    public const string Name = "Integration";
+}
